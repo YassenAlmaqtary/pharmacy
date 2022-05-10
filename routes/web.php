90d93a98ea9b7c2a779a3fcc67900c0a532fb309
YassenAlmaqtary <67117546+YassenAlmaqtary@users.noreply.github.com>
2022-05-10 20:11:00 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,47 +15,47 @@ Route:/*
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
- require __DIR__.'/auth.php';
+//  require __DIR__.'/auth.php';
 ################################# Auth User ###############################
 
 
-// Route::group(['namespace' => 'Users'], function () {
+Route::group(['namespace' => 'Users'], function () {
 
-//   //###################### bigin root ###########################
-//     Route::get('/','VendorController@index')->name('user.vendors');
-//    //###################### end root ############################
+  //###################### bigin root ###########################
+    Route::get('/','VendorController@index')->name('user.vendors');
+   //###################### end root ############################
 
-//    //##################### bagin user-auth ######################
-//     Route::get('login', 'VendorLoginController@getLogin')->name('get.user.login');
+   //##################### bagin user-auth ######################
+    Route::get('login', 'VendorLoginController@getLogin')->name('get.user.login');
  
-//     Route::post('getlogin', 'VendorLoginController@Login')->name('user.login');
+    Route::post('getlogin', 'VendorLoginController@Login')->name('user.login');
  
-//     Route::post('logout', 'VendorLoginController@logout')->name('user.logout');
+    Route::post('logout', 'VendorLoginController@logout')->name('user.logout');
 
-//     Route::resource('user', 'VendorController', [
-//       'names' => [
-//          'create' => 'user.vendors.create',
-//          'store' => 'user.vendors.store',
-//          'edit' => 'user.vendors.edit',
-//          'update' => 'user.vendors.update',
-//          'destroy' => 'user.vendors.delete',
+    Route::resource('user', 'VendorController', [
+      'names' => [
+         'create' => 'user.vendors.create',
+         'store' => 'user.vendors.store',
+         'edit' => 'user.vendors.edit',
+         'update' => 'user.vendors.update',
+         'destroy' => 'user.vendors.delete',
      
-//          ] 
-//        ]);
-//        //##################### end user-auth ######################
+         ] 
+       ]);
+       //##################### end user-auth ######################
     
-//  });
+ });
 
